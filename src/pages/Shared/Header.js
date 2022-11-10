0408d80a0dbbba55bb5872247be3fe2dc9c89be7
Safwan <safwanridwan321@gmail.com>
2022-11-10 13:44:15 +0600

    added sticky nav

--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
     }
 
     return (
-        <div className="navbar bg-base-100 shadow">
+        <div className="navbar bg-base-100 shadow sticky top-0 z-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
